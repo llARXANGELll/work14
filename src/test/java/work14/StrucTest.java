b365c8b9b86ee5ef165ee1e78bbb4d9797951701
llARXANGELll <aramh@inbox.ru>
2020-03-06 23:32:38 +0300

    асерт

--- src/test/java/work14/StrucTest.java
+++ src/test/java/work14/StrucTest.java
@@ -3,15 +3,18 @@ package work14;
 import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
-import org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class StrucTest {
 
     @Test
     public void ase() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5, 7);
+        List<Integer> numbers2 = Arrays.asList(33,43,52,64365,36,63);
         int max = Struc.findMax(numbers);
-
+        int max2 = Struc.findMax(numbers2);
+        assertThat(max).isEqualTo(7);
+        assertThat(max2).isEqualTo(64365);
 
     }
 }
