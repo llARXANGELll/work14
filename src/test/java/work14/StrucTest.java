73b91be15c04beb7e1a67cc0e5868ef0b943eed2
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-06 19:09:32 +0300

    Заработало 2

--- src/test/java/work14/StrucTest.java
+++ src/test/java/work14/StrucTest.java
@@ -10,11 +10,14 @@ public class StrucTest {
       @Test
       public void personTest (){
           List<String> prof = Arrays.asList("Pety", "Coly");
-          Person ivan = new Person("Вася", 45 , prof);
+          Person ivan = new Person("Иван", 45 , prof);
           assertThat(ivan)
+                  .as("Иван")
                   .hasFieldOrProperty("name")
                   .hasFieldOrProperty("age")
-                  .as
+                  .isInstanceOf(Person.class)
+                  .hasFieldOrPropertyWithValue("name", "Иван")
+                  .hasFieldOrPropertyWithValue("age", 45);
 
 
   }
