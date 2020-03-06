d8eb64a06bd31471f74113a242f4e35661046285
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-06 19:20:32 +0300

    Заработало 3

--- src/test/java/work14/StrucTest.java
+++ src/test/java/work14/StrucTest.java
@@ -1,24 +1,30 @@
 package work14;
 
 import org.testng.annotations.Test;
+
 import static org.assertj.core.api.Assertions.*;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class StrucTest {
 
-      @Test
-      public void personTest (){
-          List<String> prof = Arrays.asList("Pety", "Coly");
-          Person ivan = new Person("Иван", 45 , prof);
-          assertThat(ivan)
-                  .as("Иван")
-                  .hasFieldOrProperty("name")
-                  .hasFieldOrProperty("age")
-                  .isInstanceOf(Person.class)
-                  .hasFieldOrPropertyWithValue("name", "Иван")
-                  .hasFieldOrPropertyWithValue("age", 45);
+    @Test
+    public void personTest() {
+        List<String> prof = Arrays.asList("Грузчик", "Водитель", "Инженер");
+        Person ivan = new Person("Иван", 45, prof);
+        assertThat(ivan)
+                .as("Иван")
+                .hasFieldOrProperty("name")
+                .hasFieldOrProperty("age")
+                .isInstanceOf(Person.class)
+                .hasFieldOrPropertyWithValue("name", "Иван")
+                .hasFieldOrPropertyWithValue("age", 45);
+
+        assertThat(ivan.getProf())
+                .contains("Грузчик")
+                .containsExactlyInAnyOrder("Водитель", "Инженер");
 
 
-  }
+    }
 }
