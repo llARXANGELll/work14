fc0182744ff45b1be0178c3ce3000bb3c1d16aae
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-06 19:00:59 +0300

    Заработало

--- src/test/java/work14/StrucTest.java
+++ src/test/java/work14/StrucTest.java
@@ -1,7 +1,7 @@
 package work14;
 
 import org.testng.annotations.Test;
-
+import static org.assertj.core.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +11,10 @@ public class StrucTest {
       public void personTest (){
           List<String> prof = Arrays.asList("Pety", "Coly");
           Person ivan = new Person("Вася", 45 , prof);
+          assertThat(ivan)
+                  .hasFieldOrProperty("name")
+                  .hasFieldOrProperty("age")
+                  .as
 
 
   }
